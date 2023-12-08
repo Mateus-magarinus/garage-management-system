03355dcf53cff1b07dd07a472ac4024f41e4363d
Mateus Magarinus <mateusmagarinus@gmail.com>
2023-12-07 23:00:32 -0300

    Swagger for create-garage

--- apps/garageManager/src/dto/create-garage.dto.ts
+++ apps/garageManager/src/dto/create-garage.dto.ts
@@ -1,28 +1,36 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { IsBoolean, IsNumber, ValidateNested } from 'class-validator';
 
 class SizeDto {
+  @ApiProperty()
   @IsNumber()
   height: number;
 
+  @ApiProperty()
   @IsNumber()
   width: number;
 
+  @ApiProperty()
   @IsNumber()
   length: number;
 }
 
 export class CreateGarageDto {
+  @ApiProperty()
   @IsNumber()
   parkingSpace: number;
 
+  @ApiProperty()
   @IsNumber()
   priceHour: number;
 
+  @ApiProperty()
   @ValidateNested()
   @Type(() => SizeDto)
   size: SizeDto;
 
+  @ApiProperty()
   @IsBoolean()
   isCovered: boolean;
 }
